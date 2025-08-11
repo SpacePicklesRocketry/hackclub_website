15cf279022a336b938f266ee2ef075da788200bd
vardam <vardam108@gmail.com>
2025-08-10 22:45:52 -0700

    changed config again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hackclub_website',
+  base: '/hackclub_website/',
   plugins: [react()],
   server: {
     allowedHosts: true
